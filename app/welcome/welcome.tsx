251b65a7c9cc9025d1d7b0e2eff92b6b1f9e565c
germangrigoriev <ggrigoriev.000spb@gmail.com>
2025-09-13 14:26:43 +0300

    Add wordlist

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -1,45 +1,57 @@
+import { useState } from "react";
 import logoDark from "./logo-dark.svg";
 import logoLight from "./logo-light.svg";
 
 export function Welcome() {
+  const [value, setValue] = useState("");
+  const [words, setWords] = useState<string[]>([]);
+
+  const handleAdd = (event: SubmitEvent) => {
+    event.preventDefault();
+    if (value) {
+      setWords((prevWords) => [...prevWords, value]);
+      setValue("");
+    }
+  };
+
+  const handleRemove = (idx: number) => () => {
+    setWords((prevWords) => prevWords.filter((_, wordIdx) => wordIdx !== idx));
+  };
+
   return (
     <main className="flex items-center justify-center pt-16 pb-4">
       <div className="flex-1 flex flex-col items-center gap-16 min-h-0">
-        <header className="flex flex-col items-center gap-9">
-          <div className="w-[500px] max-w-[100vw] p-4">
-            <img
-              src={logoLight}
-              alt="React Router"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src={logoDark}
-              alt="React Router"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
+        <header className="flex flex-col items-center gap-9">SHLAPA</header>
         <div className="max-w-[300px] w-full space-y-6 px-4">
-          <nav className="rounded-3xl border border-gray-200 p-6 dark:border-gray-700 space-y-4">
-            <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
-              What&apos;s next?
-            </p>
-            <ul>
-              {resources.map(({ href, text, icon }) => (
-                <li key={href}>
-                  <a
-                    className="group flex items-center gap-3 self-stretch p-3 leading-normal text-blue-700 hover:underline dark:text-blue-500"
-                    href={href}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {icon}
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </nav>
+          <div className="border border-amber-200 p-2 rounded-2xl flex flex-col gap-2">
+            <h1>Word list:</h1>
+            {words.map((word, idx) => (
+              <div className="flex justify-between" key={idx}>
+                {word}
+                <button
+                  onClick={handleRemove(idx)}
+                  className="cursor-pointer  hover:text-amber-200 "
+                >
+                  X
+                </button>
+              </div>
+            ))}
+          </div>
+
+          <form onSubmit={handleAdd}>
+            <input
+              name="Word-input"
+              placeholder="Enter a word"
+              value={value}
+              onChange={(event) => setValue(event.target.value)}
+              className=" bg-amber-950"
+            />
+            <div>
+              <button className="mt-5 cursor-pointer border border-amber-200 p-2 rounded-2xl hover:text-amber-200">
+                Add entered word
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </main>
